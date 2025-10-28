048b385ccd78963251624ab9d7adfe3d4f691b05
Gerardo <ggschekaiban@hotmail.com>
2025-10-28 17:31:05 -0600

    Initial Commit

---
+++ bankapp/bankappApp.swift
@@ -0,0 +1,17 @@
+//
+//  bankappApp.swift
+//  bankapp
+//
+//  Created by Gerardo Gomez Schekaiban on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct bankappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bankappTests/bankappTests.swift
@@ -0,0 +1,17 @@
+//
+//  bankappTests.swift
+//  bankappTests
+//
+//  Created by Gerardo Gomez Schekaiban on 28/10/25.
+//
+
+import Testing
+@testable import bankapp
+
+struct bankappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
